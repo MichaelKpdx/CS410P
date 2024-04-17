4010357ce746740732b1c8edd5e4866466e9813b
MichaelKpdx <kempmic@pdx.edu>
2024-04-16 22:03:39 -0700

    Test commit and added webhell using axum

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use axum::{
 };
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(||async{"Hello, World!"}));
+    let web = Router::new().route("/", get(||async{"Hello, World!"}));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
-    axum::serve(listener,app).await.unwrap();
+    axum::serve(listener,web).await.unwrap();
 }
